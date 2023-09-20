6a0b595d68b722112bc3f14232360163e23a5901
zomborip <zombori50@gmail.com>
2023-09-20 15:36:18 +0200

    szerkesztés

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -33,6 +33,8 @@ public class GT4500 implements SpaceShip {
   *
   * @return whether at least one torpedo was fired successfully
   */
+
+  
   @Override
   public boolean fireTorpedo(FiringMode firingMode) {
 
